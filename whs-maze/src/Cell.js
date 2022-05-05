32a9dd68bc8e4abe0ae7d96036af7762a30a3bc5
Aaron <chanman2841@gmail.com>
2022-05-04 22:03:11 -0500

    setup callback from child to parent

--- whs-maze/src/Cell.js
+++ whs-maze/src/Cell.js
@@ -3,12 +3,18 @@ import { Component } from "react";
 class Cell extends Component {
     constructor() {
         super();
+
+        // bind custom method
+        this.toggleCell = this.toggleCell.bind(this)
+    }
+
+    toggleCell() {
+        this.props.getUpdate(this.props.ind[0],this.props.ind[1]);
     }
 
     render() {
-        console.log(this.props.ind)
         return(
-            <div className="cell">{this.props.ind}</div>
+            <div className="cell" onClick={this.toggleCell}>{this.props.val}</div>
         )
     }
 }
--- whs-maze/src/Grid.js
+++ whs-maze/src/Grid.js
@@ -4,8 +4,27 @@ import Cell from './Cell';
 class Grid extends Component {
     constructor() {
         super();
-        this.arr = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+        this.arr = 
+        [[1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
+        [1, 0, 1, 0, 1, 1, 1, 0, 1, 1 ],
+        [1, 1, 1, 0, 1, 1, 0, 1, 0, 1 ],
+        [0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ],
+        [1, 1, 1, 0, 1, 1, 1, 0, 1, 0 ],
+        [1, 0, 1, 1, 1, 1, 0, 1, 0, 0 ],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
+        [1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
+        [1, 1, 0, 0, 0, 0, 1, 0, 0, 1 ]]
 
+        // bind custom method
+        this.getUpdate = this.getUpdate.bind(this)
+    }
+
+    // changing state of parent component based on even in child component
+    // passing the getUpdate method to child component, which will then trigger based on an event
+    getUpdate(row,col) {
+        console.log(row,col);
+        this.arr[row][col] = 9;
+        this.forceUpdate()
     }
 
     // render the grid
@@ -16,7 +35,7 @@ class Grid extends Component {
                 {this.arr.map((row,rowInd) => {
                     return (
                         <div className="gridrow" key={rowInd}>
-                            {row.map((value,colInd) => <Cell val={value} key={rowInd.toString()+colInd.toString()} ind={[rowInd,colInd]}/>)}
+                            {row.map((value,colInd) => <Cell val={this.arr[rowInd][colInd]} key={rowInd.toString()+colInd.toString()} ind={[rowInd,colInd]} getUpdate={this.getUpdate}/>)}
                         </div>
                     )
                 })}
