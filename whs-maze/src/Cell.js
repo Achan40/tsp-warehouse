7576213b7b9494393ec8409054412629f34dfc34
Aaron <chanman2841@gmail.com>
2022-05-05 19:57:06 -0500

    setting up button toggles and cell color change callbacks

--- whs-maze/src/Cell.js
+++ whs-maze/src/Cell.js
@@ -3,18 +3,30 @@ import { Component } from "react";
 class Cell extends Component {
     constructor() {
         super();
+        this.state = {
+
+            cellColor: "white"
+        }
 
         // bind custom method
-        this.toggleCell = this.toggleCell.bind(this)
+        this.checkToggle = this.checkToggle.bind(this)
     }
 
-    toggleCell() {
-        this.props.getUpdate(this.props.ind[0],this.props.ind[1]);
+    // checking validations then call the method from the parent conditionally, change the value in the parent array by index
+    // we do this so that we can check validations at the and change the color of each individual cell
+    checkToggle() {
+        if (this.props.currentButton === "Walls" && this.props.val === 0) {
+            this.props.toggleCell(this.props.ind[0],this.props.ind[1],1);
+            this.setState({ cellColor:"black"})
+        } else if (this.props.currentButton === "Walls" && this.props.val === 1) {
+            this.props.toggleCell(this.props.ind[0],this.props.ind[1],0)
+            this.setState({ cellColor:"white"})
+        }
     }
 
     render() {
         return(
-            <div className="cell" onClick={this.toggleCell}>{this.props.val}</div>
+            <div className="cell" onClick={this.checkToggle} style={{backgroundColor:this.state.cellColor}}>{this.props.val}</div>
         )
     }
 }
--- whs-maze/src/Grid.js
+++ whs-maze/src/Grid.js
@@ -1,29 +1,41 @@
 import { Component } from "react";
-import Cell from './Cell';
+import Cell from "./Cell";
+import 'bootstrap/dist/css/bootstrap.css';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
+import ToggleButton from 'react-bootstrap/ToggleButton'
 
 class Grid extends Component {
     constructor() {
         super();
         this.arr = 
-        [[1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
-        [1, 0, 1, 0, 1, 1, 1, 0, 1, 1 ],
-        [1, 1, 1, 0, 1, 1, 0, 1, 0, 1 ],
-        [0, 0, 0, 0, 1, 0, 0, 0, 0, 1 ],
-        [1, 1, 1, 0, 1, 1, 1, 0, 1, 0 ],
-        [1, 0, 1, 1, 1, 1, 0, 1, 0, 0 ],
-        [1, 0, 0, 0, 0, 0, 0, 0, 0, 1 ],
-        [1, 0, 1, 1, 1, 1, 0, 1, 1, 1 ],
-        [1, 1, 0, 0, 0, 0, 1, 0, 0, 1 ]]
+        [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ]]
+
+        this.state = {
+            currentButton: null
+        }
 
         // bind custom method
-        this.getUpdate = this.getUpdate.bind(this)
+        this.toggleCell = this.toggleCell.bind(this)
+        this.setCurrent = this.setCurrentButton.bind(this)
     }
 
-    // changing state of parent component based on even in child component
-    // passing the getUpdate method to child component, which will then trigger based on an event
-    getUpdate(row,col) {
-        console.log(row,col);
-        this.arr[row][col] = 9;
+    // setting the current toggled button
+    setCurrentButton(val) {
+        this.setState({currentButton: val})
+    }
+
+    // changing state of parent component based on event in child component
+    // passing the toggleCell method to child component, which will then trigger (and check validations) based on an event
+    toggleCell(row, col, val) {
+        this.arr[row][col] = val
         this.forceUpdate()
     }
 
@@ -31,14 +43,25 @@ class Grid extends Component {
     // use a nested loop to generate each cell, set the key of each cell to the index of the cell in the initial array
     render() {
         return (
-            <div className="grid">
-                {this.arr.map((row,rowInd) => {
-                    return (
-                        <div className="gridrow" key={rowInd}>
-                            {row.map((value,colInd) => <Cell val={this.arr[rowInd][colInd]} key={rowInd.toString()+colInd.toString()} ind={[rowInd,colInd]} getUpdate={this.getUpdate}/>)}
-                        </div>
-                    )
-                })}
+            <div>
+                <div>{this.state.currentButton}</div>
+                <ToggleButtonGroup type="radio" name="options" onChange={(event) => {this.setCurrentButton(event);}}>
+                    <ToggleButton id="one" value={"Points"}>Place Points</ToggleButton>
+                    <ToggleButton id="two" value={"Walls"}>Place Walls</ToggleButton>
+                    <ToggleButton id="three" value={"Start"}>Set Start</ToggleButton>
+                    <ToggleButton id="three" value={"End"}>Set End</ToggleButton>
+                </ToggleButtonGroup>
+
+                <div className="grid">
+                    {this.arr.map((row,rowInd) => {
+                        return (
+                            <div className="gridrow" key={rowInd}>
+                                {row.map((value,colInd) => <Cell val={this.arr[rowInd][colInd]} key={[rowInd,colInd]} ind={[rowInd,colInd]} currentButton={this.state.currentButton} toggleCell={this.toggleCell}/>)}
+                            </div>
+                        )
+                    })}
+                </div>
+
             </div>
         )
     }
