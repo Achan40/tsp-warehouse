128b3fcd6abc5d0dbc3bb340c9f4d260ee1933fc
Aaron <chanman2841@gmail.com>
2022-05-26 23:31:03 -0500

    complete version 1

--- whs-maze/src/Intro.js
+++ whs-maze/src/Intro.js
@@ -1,6 +1,8 @@
 import { Component } from "react";
-import Container from 'react-bootstrap/Container'
-import Card from 'react-bootstrap/Card'
+import Container from 'react-bootstrap/Container';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 class Intro extends Component {
 
@@ -10,8 +12,33 @@ class Intro extends Component {
             <Container>
                 <Card bg='light' className="cust-card">
                     <Card.Body>
-                        <Card.Title>Introduction</Card.Title>
-                        <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae varius sapien. Nunc eu libero imperdiet erat efficitur pretium. Sed placerat aliquet augue, nec fermentum mauris pulvinar vel. Integer volutpat, lorem id faucibus mattis, ex augue molestie justo, nec ornare tortor libero a eros. Pellentesque in viverra dolor. Aenean faucibus ullamcorper varius. Ut quis est eget leo elementum malesuada vel a tortor. Donec rutrum ipsum a nunc lobortis, ac commodo justo posuere. Sed venenatis dignissim commodo. Sed sit amet nulla nisi. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nulla id varius augue. Pellentesque fermentum augue nec tortor facilisis hendrerit. Ut rutrum ipsum id eros convallis, sed pellentesque tellus ullamcorper.</Card.Text>
+                    <Row className="justify-content-md-center">
+                        <Col md={8}>
+                            <Card.Title>Introduction</Card.Title>
+                            <Card.Text className="intro-text">
+                                The <a class="footer-link" href="https://en.wikipedia.org/wiki/Travelling_salesman_problem" target="_blank" rel="noreferrer">Traveling Salesman Problem (TSP)</a> seeks to determine the shortest path to reach each target location exactly once and return to the starting node. 
+                                This project is my attempt at solving the TSP, and is inspired by warehouse operations at my workplace. The goal is to identify the best path to take 
+                                to pick up a number of goods, and return them not only to a set starting point, but also a desired end point if one is selected.
+                            </Card.Text>
+
+                            <Card.Title>Quick Start Guide</Card.Title>
+                            <Card.Text className="intro-text">
+                                This app is modeled after a maze. A user can select the desired nodes they want to reach in the maze traversal, and even place barriers to impede movement.
+                                A number of validations are checked upon submission, assiting the user with providing the correct inputs.
+                            </Card.Text>
+                        </Col>
+                    
+                        <Col md={6}>
+                            <ul className="bullet-list">
+                                <li>Walls and destination nodes can be placed/removed by clicking on the corresponding toggle button, and then clicking on the cell</li>
+                                <li>Start/end can only be placed/removed on an active destination node</li>
+                                <li>If no end node is selected, the final destination will default to the selected starting node</li>
+                                <li>Select the "Run TSP" button to display the results</li>
+                                <li>Hovering over a traversal path will display the path taken from one node to another on the maze.</li>
+                                <li className="bullet-warning">It is NOT RECOMMENDED to select more than 10 destination nodes</li>
+                            </ul>
+                        </Col>
+                        </Row>
                     </Card.Body>
                 </Card>
             </Container>
